0dabb592a4c9ec169c431c0b51a2899236836a53
keisuke <rememberthatk@icloud.com>
2020-04-05 19:38:07 +0900

    [master] change unconnectedapp

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import './index.css';
-import App from './components/unconnectedApp';
+import App from './components/app';
 import store from './configureStore';
 import * as serviceWorker from './serviceWorker';
 
