244debd7ac5f18453eb5a06d8fb69640ef667767
keisuke <rememberthatk@icloud.com>
2020-04-05 18:58:24 +0900

    [master] add action

---
+++ src/actions/index.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+import { getLetterMatchCount } from '../helpers';
+
+export const actionTypes = {
+  CORRECT_GUESS: 'CORRECT_GUESS',
+  GUESS_WORD: 'GUESS_WORD',
+  SET_SECRET_WORD: 'SET_SECRET_WORD',
+};
+
+export const guessWord = (guessedWord) => (dispatch, getState) => {
+  const secretWord = getState().secretWord;
+  const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
+
+  dispatch({
+    type: actionTypes.GUESS_WORD,
+    payload: { guessedWord, letterMatchCount }
+  });
+
+  if (guessedWord === secretWord) {
+    dispatch({ type: actionTypes.CORRECT_GUESS });
+  }
+};
+
+export const getSecretWord = () => dispatch => {
+  axios
+    .get('http://localhost:3001/data')
+    .then(response => {
+      dispatch({
+        type: actionTypes.SET_SECRET_WORD,
+        payload: response.answer
+      });
+    });
+}
