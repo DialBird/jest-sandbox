a1a23f828f51ee2a4bd33577e52cbdbd75256d6d
keisuke <rememberthatk@icloud.com>
2020-04-05 19:29:18 +0900

    [master] add input

---
+++ src/components/input.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { guessWord } from './actions';
+
+class Input extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { currentGuess: null }
+  }
+
+  submitGuessedWord = e => {
+    e.preventDefault();
+    const guessedWord = this.state.currentGuess;
+
+    if (guessedWord && guessedWord.length > 0) {
+      this.props.guessWord(guessedWord);
+      this.setState({ currentGuess: '' })
+    }
+  }
+
+  render() {
+    const contents = this.props.success
+      ? null
+      : (
+        <form className="form-inline">
+          <input
+            data-test="input-box"
+            className="mb-2 mx-sm-3"
+            id="word-guess"
+            type="text"
+            value={this.state.currentGuess}
+            onChange={(evt) => this.setState({ currentGuess: evt.target.value })}
+            placeholder="enter guess" />
+          <button
+            data-test="submit-button"
+            onClick={(evt) => this.submitGuessedWord(evt)}
+            className="btn btn-primary mb-2"
+            type="submit">
+            Submit
+          </button>
+        </form>
+      );
+    return (
+      <div data-test="component-input">
+        { contents }
+      </div>
+    );
+  }
+};
+
+const mapStateToProps = ({ success }) => {
+  return { success };
+}
+
+export default connect(mapStateToProps, { guessWord })(Input);
