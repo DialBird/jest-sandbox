151ad277b2c6f0caa8948cf70551af8a7eddbdf9
keisuke <rememberthatk@icloud.com>
2020-04-05 20:30:25 +0900

    [master] add testUtils

---
+++ src/testUtils.js
@@ -0,0 +1,23 @@
+import checkPropTypes from 'check-prop-types';
+import { createStore, applyMiddleware } from 'redux';
+
+import rootReducer from '../reducers';
+import { middlewares } from '../configureStore';
+
+export const storeFactory = (initialState) => {
+  const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore)
+  return createStoreWithMiddleware(rootReducer, initialState);
+}
+
+export const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`);
+}
+
+export const checkProps = (component, conformingProps) => {
+  const propError = checkPropTypes(
+    component.propTypes,
+    conformingProps,
+    'prop',
+    component.name);
+  expect(propError).toBeUndefined();
+}
